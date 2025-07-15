0841f1000d64c20e13461ce41755127d5f2b631d
luqman316 <luqmanrehan88@gmail.com>
2025-07-15 23:52:32 +0500

    luqman

--- src/app/config/mailboxlayer.ts
+++ src/app/config/mailboxlayer.ts
@@ -1,1 +1,2 @@
-export const apiKey = "78010039ed3f4fee9573b4de9d772eab"; // Your actual API key
+// export const apiKey = "78010039ed3f4fee9573b4de9d772eab";
+export const apiKey ="676569bd8d314e968f93828fbf01c1b1"
