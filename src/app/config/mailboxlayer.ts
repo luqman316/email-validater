f981fb26d496c8c2767d151e5dcaa4c98fb6f534
luqman316 <luqmanrehan88@gmail.com>
2025-07-16 15:06:52 +0500

    luqman-2

--- src/app/config/mailboxlayer.ts
+++ src/app/config/mailboxlayer.ts
@@ -1,2 +1,3 @@
 // export const apiKey = "78010039ed3f4fee9573b4de9d772eab";
-export const apiKey ="676569bd8d314e968f93828fbf01c1b1"
+// export const apiKey ="676569bd8d314e968f93828fbf01c1b1"
+export const apiKey ="bbc6bd5435c94846a2d4cc3dafa5b6dd"
